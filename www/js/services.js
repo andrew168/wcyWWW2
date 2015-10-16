acbb8ff2326e2d52a7fbd109435a31064b81c11f
andrew.zhi <andrew.zhi@mobilelive.ca>
2015-10-16 06:28:15 -0400

    规划create/load/show的API结构2: 先接口， 后私有

--- www/js/services.js
+++ www/js/services.js
@@ -1,64 +1,68 @@
 angular.module('starter')
     .factory("GetWcy", ['$http', '$localStorage',
         function($http, $localStorage) {
-        function show(sceneID) {
-            TQ.WCY.isPlayOnly = true;
-            return _load(sceneID);
-        }
-        function edit(sceneID) {
-            TQ.WCY.isPlayOnly = false;
-            return _load(sceneID);
-        }
-        function _load(sceneID) {
-            var filename = "p14959.wdm"; // straw berry
-            var content = null;
-
-            if (sceneID) {
-                filename = 'p' + sceneID + '.wdm';
+            function create() {
+                TQ.SceneEditor.createScene();
             }
 
-            var url = 'http://bone.udoido.cn/wcy/wdmOpen?filename=' + filename;
-            content = $localStorage[filename];
-            if (!content) {
-                $http.get(url, {})
-                    .success(function (data, status, headers, config) {
-                        console.log(data);
-                        content = $localStorage[filename] = JSON.stringify(data);
-                        var fileInfo = {name: filename, content: content};
-                        _open(fileInfo);
-                    }).error(function (data, status, headers, config) {
-                        console.log(data);
-                    });
-            } else {
-                var fileInfo = {name: filename, content: content};
-                _open(fileInfo);
+            function save() {
+
             }
-        }
 
-        function _open(fileinfo) {
-            $("#Container").css("width", TQ.Config.validPageWidth.toString() + "px");
-            // setStageSize(600, 480);
-            //ToDo:@UI  initCreateEnvironment(TQ.WCY.isPlayOnly);
-            TQ.SceneEditor.showWcy(fileinfo);
-            TQ.floatToolbar.initialize();
-            TQ.floatToolbar.isVisible();
-        }
+            function edit(sceneID) {
+                TQ.WCY.isPlayOnly = false;
+                return _load(sceneID);
+            }
 
-        function create() {
-            TQ.SceneEditor.createScene();
-        }
+            function show(sceneID) {
+                TQ.WCY.isPlayOnly = true;
+                return _load(sceneID);
+            }
 
-        function save() {
-        }
+            // private function:
+            function _load(sceneID) {
+                var filename = "p14959.wdm"; // straw berry
+                var content = null;
 
-        return {
-            create: create,
-            save: save,
-            edit: edit,  // open for edit
-            show: show,  // open for show only
+                if (sceneID) {
+                    filename = 'p' + sceneID + '.wdm';
+                }
 
-            // old api will be depreciated
-            test: show,
-            testCreateScene: create
-        };
-    }]);
+                var url = 'http://bone.udoido.cn/wcy/wdmOpen?filename=' + filename;
+                content = $localStorage[filename];
+                if (!content) {
+                    $http.get(url, {})
+                        .success(function (data, status, headers, config) {
+                            console.log(data);
+                            content = $localStorage[filename] = JSON.stringify(data);
+                            var fileInfo = {name: filename, content: content};
+                            _open(fileInfo);
+                        }).error(function (data, status, headers, config) {
+                            console.log(data);
+                        });
+                } else {
+                    var fileInfo = {name: filename, content: content};
+                    _open(fileInfo);
+                }
+            }
+
+            function _open(fileinfo) {
+                $("#Container").css("width", TQ.Config.validPageWidth.toString() + "px");
+                // setStageSize(600, 480);
+                //ToDo:@UI  initCreateEnvironment(TQ.WCY.isPlayOnly);
+                TQ.SceneEditor.showWcy(fileinfo);
+                TQ.floatToolbar.initialize();
+                TQ.floatToolbar.isVisible();
+            }
+
+            return {
+                create: create,
+                save: save,
+                edit: edit,  // open for edit
+                show: show,  // open for show only
+
+                // old api will be depreciated
+                test: show,
+                testCreateScene: create
+            };
+        }]);
