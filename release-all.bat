8996b8a017ea525c490aff69429c2dbc99a376f4
andrew.zhi <andrew.zhi@mobilelive.ca>
2015-12-26 14:58:34 -0500

    发布工具： 包括 正式运行根据start.bat

--- release-all.bat
+++ release-all.bat
@@ -1,3 +1,9 @@
-call release-backOffice-only.bat
-call release-wcylib-only.bat
+rem call release-backOffice-only.bat
+rem call release-wcylib-only.bat
+set src=E:\projects\WcyCore\
+set dst=E:\projects\cardforvote\
 
+copy %src%\start.bat   %dst%
+copy %src%\db-start.bat   %dst%
+rem published server to %dst%
+pause
