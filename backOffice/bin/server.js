3444ab925d1135e13749d246a48c8d05d6d60a36
andrew.zhi <andrew.zhi@mobilelive.ca>
2016-02-28 17:08:19 -0500

    !!router路由功能好了22！ 3)  必须把 route的error处理放在最后注册， 否则， 它之后注册的路径都无效

--- backOffice/bin/server.js
+++ backOffice/bin/server.js
@@ -6,13 +6,16 @@ var _config = {};
 var logger = require('./../common/logger');
 logger.config("udoido.log");
 
+console.log("//////////////////////////////");
+console.log("///      start new server  ///");
+console.log("//////////////////////////////");
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var loggerMorgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var fs = require('fs');
 
 var app = express();
 //  使用数据库的操作， 都必须在数据库启动之后， 再启动
@@ -21,26 +24,26 @@ var status = null;
 
 function onDbStarted() {
     console.info("onDbStarted...");
-    var routes = require('./../routes/index');
+    var routes = require('./../routes/index33');
     var users = require('./../routes/users');
     var getCSignature = require('./../routes/getCSignature');
     var getWSignature = require('./../routes/getWSignature');
 
-    //app.use('/', routes);
-    //app.use('/users', users);
+    app.use('/users', users);
     app.use('/getCSignature', getCSignature);
     app.use('/getWSignature', getWSignature);
+    var Wcy = require('./../routes/wcy');
+    var material = require('./../routes/material');
+    status = require('./../common/status');
 
- var Wcy = require('./../routes/wcy');
- var material = require('./../routes/material');
- status = require('./../common/status');
+    app.use('/wcy', Wcy);
+    app.use('/material', material);
+ console.log("exit at onDbStarted!");
 
- app.use('/wcy', Wcy);
- app.use('/material', material);
-    console.log("exit at onDbStarted!");
+    app.use('/index55', routes);
     init();
+    setupBaseiRoutes();
 }
-
 
 // view engine setup
 app.set('views', path.join(__dirname, './../views'));
@@ -67,51 +70,51 @@ var allowCrossDomain = function(req, res, next) {
 
 app.use(allowCrossDomain);
 
-app.use(express.static(path.join(__dirname, './../public')));
+function setupBaseiRoutes() {
+    app.use(express.static(path.join(__dirname, './../public')));
 
-app.use('/static', express.static('./www'));
+    app.use('/static', express.static('./www'));
 // 以上的路径，排除在外
 
-app.use(function(req, res, next) {
-    console.log("I'm first!!! for any path, 除了以上的路径");
-    status.checkUser(req, res);
-    next();
-});
+    app.use(function(req, res, next) {
+        console.log("I'm first!!! for any path, 除了以上的路径");
+        status.checkUser(req, res);
+        next();
+    });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+    app.use(function(req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
 
 // error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
+    if (app.get('env') === 'development') {
+        app.use(function(err, req, res, next) {
+            res.status(err.status || 500);
+            res.render('error', {
+                message: err.message,
+                error: err
+            });
+        });
+    }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {}
+        });
+    });
+}
 
 function init() {
-    console.info("/////////////////////////////");
     //DBMain.init();
     // setTimeout(DBMain.stop, 10000);
 
