f808aa30d7a3909f30d605e668c6e2c50212217e
andrew.zhi <andrew.zhi@mobilelive.ca>
2016-05-29 14:30:40 -0400

    reformat, remove extra empty line

--- gulpfile.js
+++ gulpfile.js
@@ -7,58 +7,32 @@ var $ = require('gulp-load-plugins')(); //jshint ignore:line
 var uglifyjs = require("uglify-js");
 var fs = require('fs');
 var del = require('del');
-
 var crypto = require('crypto');
-
 var args = require('yargs').argv;
-
 JSON.minify = JSON.minify || require("node-json-minify");
 
-
-
 var config = {
-
     header: null,
-
     version: null,
-
     hash: null
-
 };
 
-
-
 gulp.task('config', ['clean'], function () {
-
     //ver info
-
     config.header = "/*! ionic " + new Date().toLocaleString() + " */\n";
-
     config.version = require('./package.json').version;
-
     config.hash = config.version + "." + crypto.createHash('md5').update(config.header).digest('hex').slice(0, 8);
-
-
 
     //container files
     config.hash = "";
-
     config.app_js = "/wcy3all" + config.hash + ".js";
-
     config.app_min_js = "/wcy3all" + config.hash + ".min.js";
-
     config.app_min_js_map = "wcy3all" + config.hash + ".min.map";
-
     config.app_min_css = "/wcy3all" + config.hash + ".min.css";
-
 });
 
-
-
 gulp.task('wcylib_concat', function () {
-
     var wcylib_assets = $.useref.assets({searchPath: '{,www,www/js, www/wcy3}'});
-
     return gulp.src('www/index.html')
         .pipe(wcylib_assets)
         .pipe($.if('*.css', $.rename(config.app_min_css)))
@@ -76,33 +50,21 @@ gulp.task('wcylib_concat', function () {
         .pipe(gulp.dest(dstPath + '\\www\\lib'));
 });
 
-
-
 gulp.task('wcylib_minify', ['wcylib_concat'], function () {
-
     var result = uglifyjs.minify(["dist/" + config.app_js], {
-
         outSourceMap: config.app_min_js_map,
-
         sourceRoot: "",
-
         compress: {
-
             warnings: false
-
         }
-
     });
 
     fs.writeFileSync(dstPath + '\\www\\lib\\' + config.app_min_js, result.code);
 });
 
-
-
 gulp.task('clean', del.bind(null, ['dist', 'src/tmp']));
 
 gulp.task('build', ['wcylib_minify'], function () {
-
     return gulp.src('dist/**/*')
         .pipe($.size({title: 'build', gzip: true}))
         .pipe($.zip('ionic' + config.version + '.zip'))
