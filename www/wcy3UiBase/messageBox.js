8e8ab399fe6ff63b70722ca6df504b7e27af5869
andrew.zhi <andrew.zhi@mobilelive.ca>
2016-06-18 00:35:06 -0400

    MessageBox: 归到 UiBase模块组中

--- www/wcy3UiBase/messageBox.js
+++ www/wcy3UiBase/messageBox.js
@@ -1,12 +1,14 @@
 /**
+ * Created by Andrewz on 6/17/2016.
+ */
+/**
  * Tuqiang Game Engine
  * Copy right Tuqiang Tech
  * Created at : 12-11-12 下午4:38
  */
 
-window.TQ = window.TQ || {};
+var TQ = TQ || {};
 (function () {
-
     function MessageBox(canvas) {
         this.messageField = new createjs.Text("", "bold 24px Arial", "#FF0000");
         this.messageField.maxWidth = 1000;
@@ -17,6 +19,8 @@ window.TQ = window.TQ || {};
         stageContainer.addChild(this.messageField);
         // messageField.text = "开始载入。。。";
     }
+
+    var _instance = null;
 
     MessageBox.prototype.show = function(str) {
         if (!stageContainer.contains(this.messageField)) {
@@ -32,6 +36,27 @@ window.TQ = window.TQ || {};
     MessageBox.prototype.hide = function() {
         this.messageField.visible = false;
         stageContainer.removeChild(this.messageField);
+    };
+
+    MessageBox.getInstance = function() {
+        if (!_instance) {
+            _instance = new MessageBox(canvas);
+        }
+        return _instance;
+    };
+
+    MessageBox.show = function(msg) {
+        if (!_instance) {
+            MessageBox.getInstance();
+        }
+        _instance.show(msg);
+    };
+
+    MessageBox.hide = function(msg) {
+        if (!_instance) {
+            MessageBox.getInstance();
+        }
+        _instance.hide();
     };
 
     TQ.MessageBox = MessageBox;
