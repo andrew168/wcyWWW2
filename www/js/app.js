fa878ca5f868bb45a7b6e6e57f5bda317e04d473
andrew.zhi <andrew.zhi@mobilelive.ca>
2016-07-12 22:30:41 -0400

    没有shareCode， 也能够进入编辑

--- www/js/app.js
+++ www/js/app.js
@@ -61,5 +61,5 @@ angular.module('starter', ['ionic', 'ngStorage', 'ngCordova', 'ngCookies', 'ngFi
         ;
 
         // if none of the above states are matched, use this as the fallback
-        $urlRouterProvider.otherwise('/opus');
+        $urlRouterProvider.otherwise('/dash');
     });
