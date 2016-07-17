2aab536d05c00a6d5186d98b956f66b34b51d5a6
andrew.zhi <andrew.zhi@mobilelive.ca>
2016-07-17 11:46:51 -0400

    用支持touch的rangeslider做时间轴

--- www/wcy3UI/TimerUI.js
+++ www/wcy3UI/TimerUI.js
@@ -4,65 +4,37 @@
 
 var TQ = TQ || {};
 TQ.TimerUI = (function () {
-    var isUserControlling = false,
-        t = 0,
-        tMin = 0,
+    var t = 0,
+        tMinFrame = 0,
         tMaxFrame = 0,
-        tEle = null,
-        tMinEle = null,
-        tMaxEle = null,
-        bodyEle = null;
+        tEle = null;
 
-    var htmlStr = '<div class="toolbar-layer">' +
-        '<label id="time-slider-min-value" >0</label>' +
-        '<span>--</span>' +
-        '<label id="time-slider-max-value" >0</label>' +
-        '<div id="timer-slider-2"></div>' +
-        '<div id="maxTimeValue-2" >0</div>' +
-        '<label>time</label><input type="number" id="timeValueInput-2" >' +
+    var htmlStr =
+        '<div id="time-axis-div" class="toolbar-layer">' +
+        '<input id="timer-slider-2" atype="range" min="0" max="5" data-rangeslider>' +
+        '<output id="time-value"></output>' +
         '</div>';
-
-    return {
-        initialize: initialize
-    };
 
     function initialize () {
         t = TQ.Scene.localT2Global(TQ.FrameCounter.v);
-        tMin = 0;
+        tMinFrame = 0;
         tMaxFrame = TQ.Scene.getTMax();
         var containerDiv = document.getElementById("timer-bar-div");
         TQ.AssertExt.isNotNull(containerDiv);
         containerDiv.innerHTML = htmlStr;
-        bodyEle = $("#timer-slider-2");
         tEle = $("#timeValueInput-2");
-        tMinEle = $("#time-slider-min-value");
-        tMaxEle = $("#time-slider-max-value");
 
-        bodyEle.slider({
-            orientation: "horizontal",
-            range: "min",
-            min: tMin,
-            max: tMaxFrame,
-            value: t,
-            start: onMouseStart,
-            slide: onMouseAction,
-            change: onChange,
-            stop: onMouseStop
-        });
-
-        displayRange();
+        axis.init(t, tMinFrame, tMaxFrame);
         displayTime(t);
         TQ.FrameCounter.addHook(update);
     }
 
     function onMouseStart () {
-        isUserControlling = true;
     }
 
-    function onMouseStop (event, ui) {
-        t = ui.value;
+    function onMouseStop2 (value) {
+        t = value;
         syncToCounter();
-        isUserControlling = false;
     }
 
     function syncToCounter() {
@@ -71,32 +43,111 @@ TQ.TimerUI = (function () {
         TQ.CommandMgr.directDo(new TQ.SetTimeCommand(TQ.Scene.globalT2local(t)));
         TQ.DirtyFlag.requestToUpdateAll();
     }
-    function onMouseAction (event, ui) {
-        t = ui.value;
+
+    function onMouseAction2 (value) {
+        t = value;
         displayTime(t);
         syncToCounter();
         //ToDo: 移动时间轴的位置, 修改帧频率, 增加刻度的显示, 增加缩放
     }
 
-    function onChange () {
-        displayTime(t);
-    }
-
-    function update () {
-        if (!isUserControlling) {
+    function update () { // called by FrameCounter,
             if (TQ.FrameCounter.isNew) {
                 t = TQ.Scene.localT2Global(TQ.FrameCounter.v);
-                bodyEle.slider("value", t);
+                axis.setValue(t);
             }
-        }
     }
 
     function displayTime (t) {
-        tEle.val(t.toString());
+        var output = document.getElementById('time-value');
+        setDomEleValue(output, t);
     }
 
-    function displayRange() {
-        tMinEle.val(tMin);
-        tMaxEle.val(tMaxFrame);
+    function setDomEleValue(ele, t) {
+        if ('textContent' in document) { // For ie8 support
+            ele.textContent = t;
+        } else {
+            ele.innerText = t;
+        }
     }
+
+    var axis = (function() {
+        var bodyEle = null,
+            currentValue = 0;
+
+        return {
+            setValue: setValue,
+            init: init
+        };
+
+        function setValue(value) {
+            if (currentValue !== value) {
+                currentValue = value;
+                bodyEle.val(currentValue).change();
+            }
+        }
+
+        function init(t, tMin, tMax) {
+            var $document = $(document);
+            var selector = '[data-rangeslider]';
+            var $element = $(selector);
+
+            bodyEle = $("#timer-slider-2");
+            $document.on('input', '#timer-slider-2', onInputTimeAxis);
+            function onInputTimeAxis(e) {
+                displayTimeByEle(e.target);
+            }
+
+            // Example functionality to demonstrate a value feedback
+            function displayTimeByEle(timeAxis) {
+                var value = timeAxis.value;
+                displayTime(value);
+            }
+
+            function setupInitialValue() {
+                var attributes = {
+                    min: tMin,
+                    max: tMax,
+                    step: 1
+                };
+
+                bodyEle.attr(attributes);
+                bodyEle.rangeslider('update', true);
+            }
+
+            // Basic rangeslider initialization
+            $element.rangeslider({
+                polyfill: false,
+                onInit: onInitialized,
+                onSlide: onSlideValueChanged,
+                onSlideEnd: onSliderEnd
+            });
+
+            function onInitialized() {
+                displayTimeByEle(this.$element[0]);
+            }
+
+            function onSliderEnd(position, value) {
+                onMouseStop2(value);
+                console.log('onSlideEnd');
+                console.log('position: ' + position, 'value: ' + value);
+            }
+
+            function onSlideValueChanged(position, value) {
+                // onMouseStart(value);
+                // onMouseAction2(value);
+
+                console.log('onSlide');
+                console.log('position: ' + position, 'value: ' + value);
+            }
+
+            setupInitialValue();
+            setValue(t);
+        }
+    })();
+
+    return {
+        initialize: initialize
+    };
+
 }());
