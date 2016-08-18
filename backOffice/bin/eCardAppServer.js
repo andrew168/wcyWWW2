a37fa3b6030efab06801ca4f1e76f77be11e3e35
andrew.zhi <andrew.zhi@mobilelive.ca>
2016-08-17 20:45:41 -0400

    vHost服务器重新命名1： vServer的每一个都是oneServer的实例（就是原来的server.js)

--- backOffice/bin/eCardAppServer.js
+++ backOffice/bin/eCardAppServer.js
@@ -2,7 +2,7 @@
  * Created by Andrewz on 8/4/2016.
  */
 var appConfig = require('./eCardAppConfig.js');
-var appServer = require('./server.js');
+var appServer = require('./oneServer.js');
 
 appServer.start(appConfig);
 var app = appServer.getApp();
--- backOffice/bin/kidSaferAppServer.js
+++ backOffice/bin/kidSaferAppServer.js
@@ -2,7 +2,7 @@
  * Created by Andrewz on 8/4/2016.
  */
 var appConfig = require('./kidSaferAppConfig.js');
-var appServer = require('./server.js');
+var appServer = require('./oneServer.js');
 
 appServer.start(appConfig);
 var app = appServer.getApp();
