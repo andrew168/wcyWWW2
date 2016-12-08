d3e3a5412729c12a64154b3aad2aa0f818a4cfd6
andrew.zhi <andrew.zhi@mobilelive.ca>
2016-12-08 08:17:32 -0500

    增加toast， 确定用black 字

--- www/wcy3Base/ui/messageBox.js
+++ www/wcy3Base/ui/messageBox.js
@@ -3,21 +3,45 @@
  */
 var TQ = TQ || {};
 TQ.MessageBox = (function() {
+    var isShowingByForce = false,
+        msgList = [],
+        timer = null;
+
     var instance = {
         getInstance: getInstance,
         hide: hide,
         prompt: prompt,
         show: show,
-        showWaiting: showWaiting
+        showWaiting: showWaiting,
+        toast: toast
     };
 
     return instance;
+
+    function doShow(options) {
+        if (isShowingByForce) {
+            return msgList.push(options);
+        }
+
+        if (options.duration) {
+            isShowingByForce = true;
+            timer = setTimeout(onDuration, options.duration);
+        }
+
+        Modal.open(options);
+    }
 
     function getInstance() {
         return instance;
     }
 
     function hide() {
+        if (timer) {
+            clearTimeout(timer);
+            timer = null;
+        }
+
+        isShowingByForce = false;
         Modal.close();
     }
 
@@ -30,11 +54,29 @@ TQ.MessageBox = (function() {
     }
 
     function prompt(msg) {
-        Modal.open({content: msg, onOk: onOk, onCancel: onCancel});
+        doShow({content: msg, onOk: onOk, onCancel: onCancel});
     }
 
     function show(str) {
-        Modal.open({content: str});
+        doShow({content: str});
+    }
+
+    function toast(str) {
+        var duration = 3000;
+        doShow({content: str, duration: duration});
+    }
+
+    function onDuration() {
+        isShowingByForce = false;
+        if (msgList.length <=0) {
+            return hide();
+        }
+
+        var oldList = msgList;
+        msgList = [];
+        for (; oldList.length > 0;) {
+            doShow(oldList.pop());
+        }
     }
 
     function showWaiting(msg) {
