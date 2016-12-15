92ebf9af3f53c75db6301826f58149692dff46cd
andrew.zhi <andrew.zhi@mobilelive.ca>
2016-12-15 17:34:14 -0500

    release工具：解决bug

--- release-all.bat
+++ release-all.bat
@@ -1,7 +1,7 @@
 call release-backOffice-only.bat
 call release-wcylib-only.bat
-set src=E\projects\WcyCore\
-set dst=E:\projects\cardforvote\
+set src=E:\projects\WcyCore
+set dst=E:\projects\cardforvote
 
 copy %src%\start.bat   %dst%
 copy %src%\db-start.bat   %dst%
