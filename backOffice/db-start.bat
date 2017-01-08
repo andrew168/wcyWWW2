993f524c59219b8ec77e0e2123344e711fc781eb
andrew.zhi <andrew.zhi@mobilelive.ca>
2017-01-07 19:16:26 -0500

    @@mongoDb漏洞修补3: db-start, 一个log文件，持续append

--- backOffice/db-start.bat
+++ backOffice/db-start.bat
@@ -1,3 +1,3 @@
 rem mongod -dbpath D:\Tools\dbMongo\db
 rem  --fork=true
-mongod -dbpath D:\Tools\dbMongo\db --bind_ip=127.0.0.1 --port=27098 --fork=true --logpath=D:\Tools\dbMongo\mongod.log &
+mongod -dbpath D:\Tools\dbMongo\db --bind_ip=127.0.0.1 --port=27098 --logpath=D:\Tools\dbMongo\mongod.log --logappend &
