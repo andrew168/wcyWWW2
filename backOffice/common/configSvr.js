9052a28b6e0e43713c15bf689c13163cc78d583c
andrew.zhi <andrew.zhi@mobilelive.ca>
2017-01-09 09:21:00 -0500

    mongodb启用密码验证（需要先创建admin用户和web读写用户）

--- backOffice/common/configSvr.js
+++ backOffice/common/configSvr.js
@@ -7,9 +7,9 @@ var configSvr = {
         bindToken: "tuqiangkeji1111"
     },
 
-    // dbServer: 'mongodb://localhost:27098/test', //XX, 在断网的情况下,不能使用
-    dbServer: 'mongodb://127.0.0.1:27098/test' //  本机ip，在断网的情况下也可以使用
-    // dbServer: 'mongodb://show.udoido.cn:27098/test' //  使用统一的udoido.cn的数据库
+    // dbServer: 'mongodb://webreaderw!981:savety#$7619@localhost:27098/test', //XX, 在断网的情况下,不能使用
+    dbServer: 'mongodb://webreaderw!981:savety#$7619@127.0.0.1:27098/test' //  本机ip，在断网的情况下也可以使用
+    // dbServer: 'mongodb://webreaderw!981:savety#$7619@show.udoido.cn:27098/test' //  使用统一的udoido.cn的数据库
 };
 
 
