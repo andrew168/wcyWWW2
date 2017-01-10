604e1e06d8e6144e3aa6c17b7904b0e4afe0125b
andrew.zhi <andrew.zhi@mobilelive.ca>
2017-01-10 08:26:12 -0500

    修改mongodb端口为57098，以防止端口扫描

--- backOffice/db-start.bat
+++ backOffice/db-start.bat
@@ -1,3 +1,6 @@
 rem mongod -dbpath D:\Tools\dbMongo\db
 rem  --fork=true
-mongod --auth --dbpath D:\Tools\dbMongo\db --bind_ip=127.0.0.1 --port=27098 --logpath=D:\Tools\dbMongo\mongod.log --logappend &
+rem mongod --auth --dbpath D:\Tools\dbMongo\db --bind_ip=127.0.0.1 --port=57098 --logpath=D:\Tools\dbMongo\mongod.log --logappend &
+
+mongod --auth --dbpath D:\Tools\dbMongo\db --bind_ip=127.0.0.1 --port=57098
+rem mongod --dbpath D:\Tools\dbMongo\db --bind_ip=127.0.0.1 --port=57098
