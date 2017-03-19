f9ee2789a120a07940b81fca08a8034f59434587
andrew.zhi <andrew.zhi@mobilelive.ca>
2017-03-19 16:44:08 -0400

    气泡的anchor可以指向任意方位1： * 用 数组， * 分离建模和绘制

--- www/wcy3/graphics/graphics.js
+++ www/wcy3/graphics/graphics.js
@@ -15,6 +15,9 @@ var TQ = TQ || {};
     Graphics.drawStar = drawStar;
     Graphics.getCanvas = getCanvas;
     Graphics.setCanvas = setCanvas;
+    var ET_MOVETO = 1, // Elememt Type in polygon
+        ET_ARC = 2,
+        ET_LINE = 3;
 
     var _canvas = null;
     function getCanvas() {
@@ -49,8 +52,9 @@ var TQ = TQ || {};
 
     function drawBubble(shape, bubble) {
         //  正中心是 原点
-        drawRectBubble(shape, bubble.xmin, bubble.ymin, bubble.width, bubble.height,
-            bubble.radiusTL, bubble.radiusTR, bubble.radiusBR, bubble.radiusBL, bubble.anchor)
+        var geoModel = calBubbleModel(bubble.xmin, bubble.ymin, bubble.width, bubble.height,
+            bubble.radiusTL, bubble.radiusTR, bubble.radiusBR, bubble.radiusBL, bubble.anchor);
+        drawPolygon(shape, geoModel);
     }
 
     function drawRect(shape, x0,y0, w, h) {
@@ -77,7 +81,7 @@ var TQ = TQ || {};
             endFill();
     }
 
-    function drawRectBubble(shape, x, y, w, h, radiusTL, radiusTR, radiusBR, radiusBL, anchor) {
+    function calBubbleModel(x, y, w, h, radiusTL, radiusTR, radiusBR, radiusBL, anchor) {
         var max = (w < h ? w : h) / 2;
         var mTL = 0, mTR = 0, mBR = 0, mBL = 0,
             x2 = x + w,
@@ -108,24 +112,43 @@ var TQ = TQ || {};
             radiusBL = max;
         }
 
+        return [{type: ET_MOVETO, x: x2 - radiusTR, y: y},
+            {type: ET_ARC, x: x2 + radiusTR * mTR, y: y - radiusTR * mTR, x2: x2, y2: y + radiusTR, r: radiusTR},
+            {type: ET_LINE, x: x2, y: y2 - radiusBR},
+            {type: ET_ARC, x: x2 + radiusBR * mBR, y: y2 + radiusBR * mBR, x2: x2 - radiusBR, y2: y2, r: radiusBR},
+            {type: ET_LINE, x: anchor[0].x, y: anchor[0].y},
+            {type: ET_LINE, x: anchor[1].x, y: anchor[1].y},
+            {type: ET_LINE, x: anchor[2].x, y: anchor[2].y},
+            {type: ET_LINE, x: x + radiusTL, y: y2},
+            {type: ET_ARC, x: x - radiusBL * mBL, y: y2 + radiusBL * mBL, x2: x, y2: y2 - radiusBL, r: radiusBL},
+            {type: ET_LINE, x: x, y: y + radiusTL},
+            {type: ET_ARC, x: x - radiusTL * mTL, y: y - radiusTL * mTL, x2: x + radiusTL, y2: y, r: radiusTL}];
+    }
+
+    function drawPolygon(shape, geoModel) {
         var thickness = 1,
             edgeColor = "#000",
             fillColor = "#DDD";
-
-        shape.graphics.ss(thickness).beginStroke(edgeColor).
-            beginFill(fillColor).
-            moveTo(x2 - radiusTR, y).
-            arcTo(x2 + radiusTR * mTR, y - radiusTR * mTR, x2, y + radiusTR, radiusTR).
-            lineTo(x2, y2 - radiusBR).
-            arcTo(x2 + radiusBR * mBR, y2 + radiusBR * mBR, x2 - radiusBR, y2, radiusBR).
-            lineTo(anchor[0].x, anchor[0].y).
-            lineTo(anchor[1].x, anchor[1].y).
-            lineTo(anchor[2].x, anchor[2].y).
-            lineTo(x + radiusTL, y2).
-            arcTo(x - radiusBL * mBL, y2 + radiusBL * mBL, x, y2 - radiusBL, radiusBL).
-            lineTo(x, y + radiusTL).
-            arcTo(x - radiusTL * mTL, y - radiusTL * mTL, x + radiusTL, y, radiusTL).
-            closePath().
+        var brush = shape.graphics.ss(thickness).beginStroke(edgeColor).
+            beginFill(fillColor);
+        var i;
+        for (i = 0; i < geoModel.length; i++) {
+            var item = geoModel[i];
+            switch (item.type) {
+                case ET_MOVETO:
+                    brush.moveTo(item.x, item.y);
+                    break;
+                case ET_LINE:
+                    brush.lineTo(item.x, item.y);
+                    break;
+                case ET_ARC:
+                    brush.arcTo(item.x, item.y, item.x2, item.y2, item.r);
+                    break;
+                default:
+                    console.error("known type!", item.type);
+            }
+        }
+        brush.closePath().
             endFill();
     }
 
