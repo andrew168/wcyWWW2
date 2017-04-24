ae26aeddb7bb7b2af2a5396008b5354d3326b527
andrew.zhi <andrew.zhi@mobilelive.ca>
2017-04-24 11:39:07 -0400

    模块定义用普通对象代替函数对象： MoveCtrl （函数对象：在Safari和IE中都有bug）

--- www/wcy3/moveCtrl.js
+++ www/wcy3/moveCtrl.js
@@ -7,49 +7,57 @@
 window.TQ = window.TQ || {};
 
 (function () {
-    function MoveCtrl () {
-
-    }
-    var TO_TOP = 99999,
-        TO_BOTTOM = -99999;
-    MoveCtrl._stage = null;
-    MoveCtrl.initialize = function(aStage) {
-        MoveCtrl._stage = aStage;
-        MoveCtrl.queue = [];
-        MoveCtrl._direction = 1;
+    var MoveCtrl = {
+        initialize: initialize,
+        cmdMoveLayer: cmdMoveLayer,
+        moveLayer: moveLayer,
+        moveToTop: moveToTop,
+        moveToBottom: moveToBottom,
+        moveZ: moveZ
     };
+    var TO_TOP = 99999,
+        TO_BOTTOM = -99999,
+        _stage = null,
+        _queue = [],
+        _direction;
+
+    function initialize(aStage) {
+        _stage = aStage;
+        _queue.splice(0);
+        _direction = 1;
+    }
 
     // 连续Z向移动， 距离越远， 移动的越多。
     // 与鼠标运动快慢， 一致。
-    MoveCtrl._accumulateStep = 0;
-    MoveCtrl._lastItemID = -1;
+    var _accumulateStep = 0,
+        _lastItemID = -1;
 
     $(document).mouseup(function () {
-        MoveCtrl._accumulateStep = 0;
+        _accumulateStep = 0;
     });
 
-    MoveCtrl.isSameItem = function(target) {
-        return (MoveCtrl._lastItemID == target.id);
-    };
+    function isSameItem(target) {
+        return (_lastItemID == target.id);
+    }
 
-    MoveCtrl.moveZ = function (ele, offset, ev) {
+    function moveZ(ele, offset, ev) {
         var target = ele.displayObj;
         // offset 是 hit点与图像定位点之间的偏移， 在MouseDown的时候由Element的onPress计算的
         var deltaY = TQ.Utility.deltaYinWorld(target, offset, ev);
         var step = Math.floor(deltaY /TQ.Config.MouseSensitivity);
-        var deltaStep = (MoveCtrl.isSameItem(target))? (step - MoveCtrl._accumulateStep) : step;
+        var deltaStep = (isSameItem(target))? (step - _accumulateStep) : step;
         if (deltaStep != 0) {
-            MoveCtrl._accumulateStep = step;
-            MoveCtrl._doMoveZ(ele, deltaStep);
-            TQ.Log.out("ID:" + MoveCtrl._lastItemID + "sum" + MoveCtrl._accumulateStep
+            _accumulateStep = step;
+            _doMoveZ(ele, deltaStep);
+            TQ.Log.out("ID:" + _lastItemID + "sum" + _accumulateStep
                 +", step: " + step + ", delta: " + deltaStep);
         }
-    };
+    }
 
     /*
     移动层次，step >= 1： 向上移动1层； step <-1： 向下移动1层
      */
-    MoveCtrl.moveLayer = function (ele, step) {
+    function moveLayer(ele, step) {
         var oldZ = (step >0) ? ele.getMaxZ(): ele.getMinZ();  // 防止，目标z落在自身
         if ((oldZ <= 0) && (step <=0)) { // 已经是最底层， 不能再move了
             TQ.MessageBox.toast("already in lowest layer!");
@@ -75,106 +83,106 @@ window.TQ = window.TQ || {};
     }
 
     // 下面的函数只被command所调用, 不会被其它函数调用
-    MoveCtrl.cmdMoveLayer = function (ele, step) {
+    function cmdMoveLayer(ele, step) {
         assertNotNull(TQ.Dictionary.FoundNull, ele);
         if (!ele) return;
-        MoveCtrl._openQueue(step);
-        MoveCtrl._doMoveZ(ele, step);
-        MoveCtrl._flush();
-    };
+        _openQueue(step);
+        _doMoveZ(ele, step);
+        _flush();
+    }
 
     /*
     移动到最顶层
      */
-    MoveCtrl.moveToTop = function (ele) {
+    function moveToTop(ele) {
         assertNotNull(TQ.Dictionary.FoundNull, ele);
         if (!ele) return;
-        MoveCtrl.moveLayer(ele, TO_TOP);
-    };
+        moveLayer(ele, TO_TOP);
+    }
 
     /*
      移动到最底层
      */
-    MoveCtrl.moveToBottom = function (ele) {
+    function moveToBottom(ele) {
         assertNotNull(TQ.Dictionary.FoundNull, ele);
         if (!ele) return;
-        MoveCtrl.moveLayer(ele, TO_BOTTOM);
-    };
+        moveLayer(ele, TO_BOTTOM);
+    }
 
-    MoveCtrl._doMoveZ = function (ele, step) {
+    function _doMoveZ(ele, step) {
         var target = ele.displayObj;
         // move up the selected object toward more visible
         if (null != target) {
-            MoveCtrl._moveZOne(ele);
-            MoveCtrl._lastItemID = target.id;
+            _moveZOne(ele);
+            _lastItemID = target.id;
             if (!!ele.children) {
                 for (var i=0; i< ele.children.length; i++) {
                     var child = ele.children[i];
                     if (!child.isMarker()) {
-                        MoveCtrl._doMoveZ(child, step);
+                        _doMoveZ(child, step);
                     }
                 }
             }
         }
-    };
+    }
 
-    MoveCtrl._moveZOne = function(ele)
+    function _moveZOne(ele)
     {
         if (!ele.displayObj) return;
-        var id = MoveCtrl._stage.getChildIndex(ele.displayObj);
+        var id = _stage.getChildIndex(ele.displayObj);
         if (id >= 0) {
-            MoveCtrl.queue.push({"id": id, "ele":ele});
+            _queue.push({"id": id, "ele":ele});
         }
-    };
+    }
 
-    MoveCtrl._openQueue = function(step) {
-        MoveCtrl._direction = step;
-        MoveCtrl.queue.splice(0);
-    };
+    function _openQueue(step) {
+        _direction = step;
+        _queue.splice(0);
+    }
 
-    MoveCtrl._flush = function() {
-        var num = MoveCtrl.queue.length;
+    function _flush() {
+        var num = _queue.length;
         if (num > 0) {
-            if (MoveCtrl._direction < 0) {
-                MoveCtrl.queue.sort(function(a, b) {return a.id >= b.id;})
+            if (_direction < 0) {
+                _queue.sort(function(a, b) {return a.id >= b.id;})
             } else {
-                MoveCtrl.queue.sort(function(a, b) {return a.id <= b.id;})
+                _queue.sort(function(a, b) {return a.id <= b.id;})
             }
-            var step = MoveCtrl._direction;
+            var step = _direction;
             // 上移一层但是已经到顶，或者下移一层但是已经到底， 就不再操作）
-            if ( (step == 1) && ((MoveCtrl._stage.getNumChildren() - 1) == MoveCtrl.queue[0].id)) {return; }
-            if ( (step == -1) && (0 == MoveCtrl.queue[0].id)) {return; }
+            if ( (step == 1) && ((_stage.getNumChildren() - 1) == _queue[0].id)) {return; }
+            if ( (step == -1) && (0 == _queue[0].id)) {return; }
             // 到底、到顶操作：确保各个子元素的移动距离是一样的， 不能都奔到最顶最低
             if (step === TO_TOP) {
-                step = (MoveCtrl._stage.getNumChildren() - 1) - MoveCtrl.queue[0].id;
+                step = (_stage.getNumChildren() - 1) - _queue[0].id;
             } else if (step === TO_BOTTOM) {
-                step = - MoveCtrl.queue[0].id;
+                step = - _queue[0].id;
             }
             if (step == 0) return;
             for (var i = 0; i < num; i ++) {
-                var item = MoveCtrl.queue.shift();
-                MoveCtrl._doMoveZOne(item.ele, step);
+                var item = _queue.shift();
+                _doMoveZOne(item.ele, step);
             }
         }
-    };
+    }
 
-    MoveCtrl._doMoveZOne = function(ele, step)
+    function _doMoveZOne(ele, step)
     {
         var target = ele.displayObj;
         if (!target) return;
-        var id = MoveCtrl._stage.getChildIndex(target);
+        var id = _stage.getChildIndex(target);
         if (id >= 0) {
-            var newID = TQ.MathExt.range(id + step, 0, MoveCtrl._stage.getNumChildren() - 1);
+            var newID = TQ.MathExt.range(id + step, 0, _stage.getNumChildren() - 1);
             if (id != newID) {
                 if ((step > 1) || (step < -1))  { // move to Top, or Bottom
-                    MoveCtrl._stage.setChildIndex(ele.displayObj, newID);
+                    _stage.setChildIndex(ele.displayObj, newID);
                 } else {
-                    MoveCtrl._stage.swapChildrenAt(id, newID);
+                    _stage.swapChildrenAt(id, newID);
                 }
             }
         }
         TQ.DirtyFlag.setElement(ele);
-    };
+    }
 
     TQ.MoveCtrl = MoveCtrl;
 }) ();
