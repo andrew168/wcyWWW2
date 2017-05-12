e93c1a50c1895a2e3e8823b8361805a58d6ba58a
andrew.zhi <andrew.zhi@mobilelive.ca>
2017-05-12 07:25:00 -0400

    缺省配置： 不抠图； 如果扣，则全扣，而不是只去除白边

--- www/wcy3Log/Config.js
+++ www/wcy3Log/Config.js
@@ -113,6 +113,7 @@ window.TQ = window.TQ || {};
     Config.hightlightOn = true;
     Config.useHighlightBox = false;  // 不显示red的亮显box
     Config.koutuOn = false; // 默认是不抠图， 如果打开，则自动抠图， 并且自动剔除周边的空白
+    Config.removeWhiteMarginOnly = false; // 在抠图的情况下， 只去除空白边，比全面抠图
     Config.textBubbleOn = false;
     Config.useCreateJSFullContainer = false; // 不把ele放到group container的displayObj中， 而是仍然放在stageContainer中
     Config.ignoreCachedFile = true;  //忽略保存在cache中的文件数据， 等用户ID系统正常使用之后， 再考虑如何启用它。
