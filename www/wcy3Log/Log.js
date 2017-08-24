8f9132d911b4ecfeb749431f4be9053b5267b665
andrew.zhi <andrew.zhi@mobilelive.ca>
2017-08-24 08:38:11 -0400

    Refactor: 用singleton模式改写， 这样才支持编辑器的智能提示函数名，

--- www/wcy3Log/Log.js
+++ www/wcy3Log/Log.js
@@ -4,69 +4,86 @@
  * 移动操作器
  */
 
-window.TQ = window.TQ || {};
+var TQ = TQ || {};
 
-(function () {
-    var Log =function () {
+TQ.Log = (function () {
+    var self = {
+        CLOSE: 0,
+        CRITICAL_LEVEL: 1,
+        ERROR_LEVEL: 2, // 代替 assert 的throw
+        WARN_LEVEL: 5,
+        INFO_LEVEL: 7, // 用于 跟踪调试, 查看软件的执行过程
+
+        upgrade: upgrade,
+        alertInfo: alertInfo,
+        alertError: alertError,
+        matrixDebugInfo: matrixDebugInfo,
+        tsrDebugInfo: tsrDebugInfo,
+        debugInfo: debugInfo,
+        depreciated: depreciated,
+        warn: warn,
+        error: error,
+        criticalError: criticalError,
+        trace: trace,
+        setLevel: setLevel,
+
+        open: open,
+        close: close
     };
 
-    Log.CLOSE = 0;
-    Log.CRITICAL_LEVEL = 1;
-    Log.ERROR_LEVEL = 2; // 代替 assert 的throw
-    Log.WARN_LEVEL = 5;
-    Log.INFO_LEVEL = 7; // 用于 跟踪调试, 查看软件的执行过程
-    Log.level = TQ.Config.LOG_LEVEL;
-    Log.open = function () { Log.level = Log.INFO_LEVEL;};
-    Log.close = function () {Log.level = Log.CLOSE;};
-    Log.setLevel = function(level) { Log.level = level;};
-    Log.trace = function (str) {  //  只用于跟踪调试, (改info为trace), 不能直接出现在 release版中,
+    var logLevel = TQ.Config.LOG_LEVEL;
+
+    function open() { logLevel = self.INFO_LEVEL;}
+    function close() {logLevel = self.CLOSE;}
+    function setLevel(level) { logLevel = level;}
+    function trace(str) {  //  只用于跟踪调试, (改info为trace), 不能直接出现在 release版中,
       console.log(str);
-    };
+    }
 
-    Log.criticalError = function (str) {
-        if (Log.level >= Log.CRITICAL_LEVEL) console.error(str);
-    };
+    function criticalError(str) {
+        if (level >= self.CRITICAL_LEVEL) console.error(str);
+    }
 
-    Log.error = function (str) {
-        if (Log.level >= Log.ERROR_LEVEL) console.error(str);
-    };
+    function error(str) {
+        if (logLevel >= self.ERROR_LEVEL) console.error(str);
+    }
 
-    Log.warn = function (str) {
-        if (Log.level >= Log.WARN_LEVEL) console.warn(str);
-    };
+    function warn(str) {
+        if (logLevel >= self.WARN_LEVEL) console.warn(str);
+    }
 
-    Log.depreciated = function (str) {
+    function depreciated(str) {
         if (TQ.Config.depreciateCheckOn) {
             if (!str) {
                 str = "";
             }
             console.error("this is depreciated. " + str);
         }
-    };
+    }
 
-    Log.debugInfo = function (str) {
+    function debugInfo(str) {
         console.info(str);
-    };
+    }
 
-    Log.tsrDebugInfo = function (msg, obj) {
-        Log.debugInfo(msg + "("+ obj.x.toFixed(2) + "," + obj.y.toFixed(2) + "), Scale(" +
+    function tsrDebugInfo(msg, obj) {
+        debugInfo(msg + "("+ obj.x.toFixed(2) + "," + obj.y.toFixed(2) + "), Scale(" +
             obj.sx.toFixed(2) + "," + obj.sy.toFixed(2) + "), A:" + obj.rotation.toFixed(2));
-    };
+    }
 
-    Log.matrixDebugInfo = function(msg, m) {
-        Log.debugInfo(msg + "matrix translation: " + m.elements[0][2].toFixed(20) + ", " +
+    function matrixDebugInfo(msg, m) {
+        debugInfo(msg + "matrix translation: " + m.elements[0][2].toFixed(20) + ", " +
             m.elements[1][2].toFixed(20) + " " + m.elements[2][2].toFixed(20));
-    };
+    }
 
-    if (Log.level >= Log.INFO_LEVEL) {
-        Log.info = Log.out = function(str) {
+    if (logLevel >= self.INFO_LEVEL) {
+        self.info = self.out = function(str) {
             console.log(str);
         };
     } else {
-        Log.debugInfo = Log.info = Log.out = function() {};
+        self.debugInfo = self.info = self.out = function() {};
     }
 
-    Log.alertError = function (str) {
+    function alertError(str) {
         // 主要是 debug 微信的程序使用
         if (typeof str != "string") {
             str = JSON.stringify(str);
@@ -74,11 +91,10 @@ window.TQ = window.TQ || {};
         if (TQ.Config.hasWx && TQ.Config.WX_DEBUG_ENABLED) {
             alert("wx调试__" + str);
         } else {
-            Log.error(str);
+            error(str);
         }
-    };
-
-    Log.alertInfo = function (str) {
+    }
+    function alertInfo(str) {
         // 主要是 debug 微信的程序使用
         if (typeof str != "string") {
             str = JSON.stringify(str);
@@ -86,16 +102,16 @@ window.TQ = window.TQ || {};
         if (TQ.Config.hasWx && TQ.Config.WX_DEBUG_ENABLED) {
             alert("wx调试__" + str);
         } else {
-            Log.info(str);
+            info(str);
         }
-    };
+    }
 
-    Log.upgrade = function (str) {
+    function upgrade(str) {
         if (!str) {
             str = "";
         }
         console.error("必须升级到：" + str);
-    };
+    }
 
-    TQ.Log = Log;
+    return self;
 }) ();
