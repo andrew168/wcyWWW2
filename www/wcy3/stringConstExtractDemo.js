caed49d2bda54e44626c3032de952af03f8b8383
andrew.zhi <andrew.zhi@mobilelive.ca>
2017-09-14 09:07:19 -0400

    Locale:测试文件

---
+++ www/wcy3/stringConstExtractDemo.js
@@ -0,0 +1,16 @@
+
+window.TQ = window.TQ || {};
+
+TQ.MoveCtrl = (function () {
+    var _self = {
+        initialize: initialize,
+    };
+
+    function initialize() {
+        TQ.MessageBox.toast(TQ.Locale.getStr('already in lowest layer!'));
+        TQ.MessageBox.toast(TQ.Locale.getStr("couldn't move any more！"));
+        TQ.MessageBox.prompt(TQ.Locale.getStr('screenshot uploaded successfully!'));
+    }
+
+    return _self;
+})();
