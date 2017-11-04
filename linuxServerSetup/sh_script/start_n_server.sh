5d5765f7d96fc20c322283615c7eb1e91f9cff45
andrew.zhi <andrew.zhi@mobilelive.ca>
2017-11-04 09:19:20 -0400

    !!!恢复启动服务器：3 * node 版本混乱， 统一使用不带路径的node （删除老版本） (这个文件还需要吗？，需要测试）

--- linuxServerSetup/sh_script/start_n_server.sh
+++ linuxServerSetup/sh_script/start_n_server.sh
@@ -10,7 +10,7 @@ pgrep node || not_found=1
 
 if [ ${not_found} = 1 ] ; then
     echo "not found node"
-    nohup /usr/local/node/bin/node  /data/wwwz/card2/backoffice/bin/server.js  &
+    nohup node  /data/wwwz/card2/backoffice/bin/server.js  &
 else
     echo "find node"
 fi
