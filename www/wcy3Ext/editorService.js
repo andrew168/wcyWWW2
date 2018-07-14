a4cd2e9bad1d7bfdd100423f4d831f453e51fce0
andrew.zhi <andrew.zhi@mobilelive.ca>
2018-07-14 07:32:10 -0400

    翻译：至少要有一个场景!

--- www/wcy3Ext/editorService.js
+++ www/wcy3Ext/editorService.js
@@ -759,7 +759,7 @@ function EditorService($q, $rootScope, $timeout, NetService, WxService, WCY, App
             // $timeout(function() {
             //     deleteLevel(0);
             // });
-            return TQ.MessageBox.prompt('至少要有一个场景!');
+            return TQ.MessageBox.prompt('at least 1 scene!');
         }
 
         assertNotNull(TQ.Dictionary.FoundNull, currScene); // 必须在微创意显示之后使用
