82b28bc6bbb2a23f6f781154d13daec3c4c0aba9
andrew.zhi <andrew.zhi@mobilelive.ca>
2018-08-05 14:22:41 -0400

    关联主题到素材2：取消关联

--- backOffice/routes/material.js
+++ backOffice/routes/material.js
@@ -87,6 +87,29 @@ router.post('/attachTopic', authHelper.ensureAuthenticated, function (req, res, 
     }
 });
 
+router.post('/detachTopic', authHelper.ensureAuthenticated, function (req, res, next) {
+    var matPublic_id = req.body.public_id || null,
+        topicId = req.body.topicId || null,
+        matType = getMatType(req),
+        matId = utils.matName2Id(matPublic_id),
+        user = status.getUserInfo(req, res);
+
+    if (!user) {
+        return netCommon.notLogin(req, res);
+    }
+
+    status.logUser(user, req, res);
+    getMatController(matType).detachTopic(matType, matId, topicId, user, onSuccess, onError);
+
+    function onSuccess(id, doc) {
+        res.json(doc);
+    }
+
+    function onError(err) {
+        res.json(err);
+    }
+});
+
 router.get('/', function(req, res, next) {
     console.log("params: " + JSON.stringify(req.params));
     console.log("body: " + JSON.stringify(req.body));
