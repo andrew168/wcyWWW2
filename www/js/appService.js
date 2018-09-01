00bedd52f7603beb528b316bb3b23da62f795f0e
andrew.zhi <andrew.zhi@mobilelive.ca>
2018-09-01 19:59:37 -0400

    纯refactor

--- www/js/appService.js
+++ www/js/appService.js
@@ -9,25 +9,25 @@ AppService.$inject = ['$stateParams', '$timeout', 'WCY', 'NetService', 'DeviceSe
 function AppService($stateParams, $timeout, WCY, NetService, DeviceService,
               Setup, UserService) {
 
-        var STATE_LOADED = 1,
-            STATE_STARTING = 2,
-            STATE_STARTED = 3;
-        var _state = STATE_LOADED,
-            _initialized = false,
-            _onAppStarting = null,
-            _onAppStarted = null; // onAppStartDefault;
+    var STATE_LOADED = 1,
+        STATE_STARTING = 2,
+        STATE_STARTED = 3;
+    var _state = STATE_LOADED,
+        _initialized = false,
+        _onAppStarting = null,
+        _onAppStarted = null; // onAppStartDefault;
 
-        function configCanvas() {
-            TQ.State.updateDeviceInfo();
-            TQ.State.determineWorkingRegion();
-            TQ.Graphics.setCanvas();
-            if (currScene) {
-                TQ.DirtyFlag.setScene();
-            }
-
-            TQ.Log.debugInfo("scren is: (" + TQ.Config.workingRegionWidth + ", " + TQ.Config.workingRegionHeight +")"
-                + "orientation = " + TQ.Config.orientation);
+    function configCanvas() {
+        TQ.State.updateDeviceInfo();
+        TQ.State.determineWorkingRegion();
+        TQ.Graphics.setCanvas();
+        if (currScene) {
+            TQ.DirtyFlag.setScene();
         }
+
+        TQ.Log.debugInfo("scren is: (" + TQ.Config.workingRegionWidth + ", " + TQ.Config.workingRegionHeight + ")"
+            + "orientation = " + TQ.Config.orientation);
+    }
 
     function _init() {
         if (UserService.canAutoLogin()) {
@@ -44,105 +44,106 @@ function AppService($stateParams, $timeout, WCY, NetService, DeviceService,
         //remove_debugger_end
 
         if (_initialized) {
-                TQ.Log.error("Duplicated call in _init");
-                return;
-            }
-            if (!TQ.Utility.isSupportedEnvironment()) {
-                var msg = TQ.Locale.getStr('please use android mobile');
-                // +' <a href="https://www.google.ca/chrome/browser/features.html" style="font-weight: bold; text-decoration: underline">' +
-                    // TQ.Locale.getStr('chrome') + '</a>！';
-                TQ.MessageBox.prompt(msg, function () {
-                    msg = TQ.Locale.getStr('please copy this page to technical support') + ': support@udoido.com. \n\n\n\r\n\r';
-                    $timeout(TQ.MessageBox.prompt(msg + navigator.userAgent +' \n\r' + navigator.vendor));
-                });
-                return;
-            }
-            _initialized = true;
-            TQ.Log.debugInfo("_init");
-            configCanvas();
-            if (TQ.Config.LocalCacheEnabled) {
-                document.addEventListener(TQ.EVENT.FILE_SYSTEM_READY, onFileSystemReady, false);
-                DeviceService.initialize();
-            } else {
-                onFileSystemReady();
-            }
-            TQ.Log.checkPoint('file system ready');
-            if (!TQ.Env || !TQ.Env.lang) {
-                TQ.Log.error('must setup default language!');
-            }
-            TQ.Locale.initialize(TQ.Env.lang);
-            if (_state < STATE_STARTING) { // 由于autoLogin的影响， 可能此段函数被滞后了。
-                _state = STATE_STARTING
-            }
-
-            if (_onAppStarting) {
-                _onAppStarting();
-            }
+            TQ.Log.error("Duplicated call in _init");
+            return;
+        }
+        if (!TQ.Utility.isSupportedEnvironment()) {
+            var msg = TQ.Locale.getStr('please use android mobile');
+            // +' <a href="https://www.google.ca/chrome/browser/features.html" style="font-weight: bold; text-decoration: underline">' +
+            // TQ.Locale.getStr('chrome') + '</a>！';
+            TQ.MessageBox.prompt(msg, function () {
+                msg = TQ.Locale.getStr('please copy this page to technical support') + ': support@udoido.com. \n\n\n\r\n\r';
+                $timeout(TQ.MessageBox.prompt(msg + navigator.userAgent + ' \n\r' + navigator.vendor));
+            });
+            return;
+        }
+        _initialized = true;
+        TQ.Log.debugInfo("_init");
+        configCanvas();
+        if (TQ.Config.LocalCacheEnabled) {
+            document.addEventListener(TQ.EVENT.FILE_SYSTEM_READY, onFileSystemReady, false);
+            DeviceService.initialize();
+        } else {
+            onFileSystemReady();
+        }
+        TQ.Log.checkPoint('file system ready');
+        if (!TQ.Env || !TQ.Env.lang) {
+            TQ.Log.error('must setup default language!');
+        }
+        TQ.Locale.initialize(TQ.Env.lang);
+        if (_state < STATE_STARTING) { // 由于autoLogin的影响， 可能此段函数被滞后了。
+            _state = STATE_STARTING
         }
 
-        // 三个阶段： DeveiceReady, DOM ready, ImageCacheReady, DirReady
-        function onFileSystemReady() {
-            if (TQ.Config.LocalCacheEnabled) {
-                document.addEventListener(TQ.EVENT.DIR_READY, onDirReady, false);
-                document.removeEventListener(TQ.EVENT.FILE_SYSTEM_READY, onFileSystemReady);
-                Setup.initialize();
-                NetService.initialize();
-            } else {
-                NetService.initialize();
-                onDirReady();
-            }
+        if (_onAppStarting) {
+            _onAppStarting();
         }
+    }
 
-        function onDirReady() {
-            if (TQ.Config.LocalCacheEnabled) {
-                document.removeEventListener(TQ.EVENT.DIR_READY, onDirReady);
-                assertTrue("device要先ready", DeviceService.isReady());
-            }
-            if (_onAppStarted) {
-                $timeout(function () {
-                    _onAppStarted();
-                });
-            }
-            _state = STATE_STARTED;
+    // 三个阶段： DeveiceReady, DOM ready, ImageCacheReady, DirReady
+    function onFileSystemReady() {
+        if (TQ.Config.LocalCacheEnabled) {
+            document.addEventListener(TQ.EVENT.DIR_READY, onDirReady, false);
+            document.removeEventListener(TQ.EVENT.FILE_SYSTEM_READY, onFileSystemReady);
+            Setup.initialize();
+            NetService.initialize();
+        } else {
+            NetService.initialize();
+            onDirReady();
         }
+    }
 
-        function onAppStartDefault() {
-            // $scope.testDownload();
-
-            var opus = TQ.Utility.getUrlParam('opus');
-            if (!opus && $stateParams.shareCode) {
-                opus = $stateParams.shareCode;
-            }
-            // opus = "100_12345678_123_1234567890";
-            // opus = "100_00000016_123_1234567890";
-            // opus = "100_00000025_123_1234567890";
-            if (opus) {
-                WCY.getWcy(opus);
-            } else {
-                WCY.start();
-            }
-            //WxService.init();
-            // $cordovaProgress.hide();
+    function onDirReady() {
+        if (TQ.Config.LocalCacheEnabled) {
+            document.removeEventListener(TQ.EVENT.DIR_READY, onDirReady);
+            assertTrue("device要先ready", DeviceService.isReady());
         }
-
-        function setOnAppStarted(fn) {
-            _onAppStarted = fn;
-            if (_state === STATE_STARTED) {
+        if (_onAppStarted) {
+            $timeout(function () {
                 _onAppStarted();
-            }
+            });
         }
+        _state = STATE_STARTED;
+    }
 
-        function setOnAppStarting(fn) {
-            _onAppStarting = fn;
-            if (_state >= STATE_STARTING) {
-                _onAppStarting();
-            }
-        }
+    function onAppStartDefault() {
+        // $scope.testDownload();
 
-        return {
-            init: _init,
-            configCanvas: configCanvas,
-            onAppStarting: setOnAppStarting,
-            onAppStarted: setOnAppStarted
+        var opus = TQ.Utility.getUrlParam('opus');
+        if (!opus && $stateParams.shareCode) {
+            opus = $stateParams.shareCode;
         }
+        // opus = "100_12345678_123_1234567890";
+        // opus = "100_00000016_123_1234567890";
+        // opus = "100_00000025_123_1234567890";
+        if (opus) {
+            WCY.getWcy(opus);
+        } else {
+            WCY.start();
+        }
+        //WxService.init();
+        // $cordovaProgress.hide();
+    }
+
+    function setOnAppStarted(fn) {
+        _onAppStarted = fn;
+        if (_state === STATE_STARTED) {
+            _onAppStarted();
+        }
+    }
+
+    function setOnAppStarting(fn) {
+        _onAppStarting = fn;
+        if (_state >= STATE_STARTING) {
+            _onAppStarting();
+        }
+    }
+
+    return {
+        init: _init,
+        configCanvas: configCanvas,
+        onAppStarting: setOnAppStarting,
+        onAppStarted: setOnAppStarted
+    }
 }
+
