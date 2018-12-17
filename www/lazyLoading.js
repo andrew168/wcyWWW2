5c04c0e52bde73012a6d185e8719d607c96b36f6
andrew.zhi <andrew.zhi@mobilelive.ca>
2018-12-16 22:40:56 -0500

    lazy load, 支持image和audio了

--- www/lazyLoading.js
+++ www/lazyLoading.js
@@ -2,35 +2,57 @@
  * Created by Andrewz on 7/12/2016.
  */
 
-var TQ = TQ ||{};
-TQ.LazyLoading = (function(){
-    return {
-        loadOne: loadOne
-    };
+var TQ = TQ || {};
+TQ.LazyLoading = (function () {
+  var tagType = {
+      css: 'link',
+      js: 'script',
+      jpg: 'img',
+      png: 'img',
+      mp3: 'audio'
+    },
+    d = document;
 
-    function loadOne(src, onLoaded) {
-        var d = document,
-            s = (src.indexOf(".css") < 0) ? 'script' : 'link',
-            id = src.replace(/\W/g, '_');
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {
-            return;
-        }
-        js = d.createElement(s);
-        js.id = id;
-        if (s === 'link') {
-            document.getElementsByTagName('head')[0].appendChild(js);
-        } else {
-            fjs.parentNode.insertBefore(js, fjs);
-        }
+  return {
+    loadOne: loadOne
+  };
 
-        js.onload = onLoaded;
-        if (s === 'link') {
-            js.href = src;
-            js.rel = 'stylesheet';
-            js.type = 'text/css';
-        } else {
-            js.src = src;
-        }
+  function loadOne(src, onLoaded) {
+    var words = src.split('.'),
+      ext = words[words.length - 1].toLowerCase(),
+      tag = tagType[ext],
+      id = src.replace(/\W/g, '_');
+    var ele, fjs = d.getElementsByTagName(tag)[0],
+      parent = d.body;
+    if (d.getElementById(id)) {
+      return;
     }
+    ele = d.createElement(tag);
+    ele.id = id;
+    ele.onload = onLoaded;
+
+    switch (tag) {
+      case 'link':
+        ele.href = src;
+        ele.rel = 'stylesheet';
+        ele.type = 'text/css';
+        d.getElementsByTagName('head')[0].appendChild(ele);
+        break;
+      case 'script':
+        ele.src = src;
+        fjs.parentNode.insertBefore(ele, fjs);
+        parent.appendChild(ele);
+        break;
+      case 'img':
+        ele.src = src;
+        ele.style.visibility = 'hidden';
+        parent.appendChild(ele);
+        break;
+      case 'audio':
+        ele.src = src;
+        parent.appendChild(ele);
+        break;
+      default:
+    }
+  }
 })();
