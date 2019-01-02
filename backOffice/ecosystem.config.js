fc14634908780e962f3d70f0fc93b7298273eaf6
andrew.zhi <andrew.zhi@mobilelive.ca>
2019-01-02 18:49:54 -0500

    pm2并行多cluster配置

--- backOffice/ecosystem.config.js
+++ backOffice/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: 'myApp1',
-    script: 'bin\\server.js',
+    script: 'bin/server.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     args: 'one two',
