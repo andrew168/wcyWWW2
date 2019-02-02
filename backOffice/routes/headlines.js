6531ceed9cf3fee86481000c9056f4a84d0c181e
andrew.zhi <andrew.zhi@mobilelive.ca>
2019-02-02 09:27:45 -0500

    非教师，也可以用

--- backOffice/routes/headlines.js
+++ backOffice/routes/headlines.js
@@ -4,8 +4,8 @@
 var express = require('express'),
   router = express.Router();
 
-var headlines = ['祝老师新年愉快，阖家欢乐！',
-  'BONE教师邦参加教育创变者大会--“进深”',
+var headlines = ['祝您新年愉快，阖家欢乐！',
+  'BONE教师邦参加教育创变者大会--“进深”， 四川 成都，2019.1.16',
   '图话--用引擎技术助力教育创新'
 ];
 
