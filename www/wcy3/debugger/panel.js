28a34360476f224f103de99864cb206a0cd77b93
andrew.zhi <andrew.zhi@mobilelive.ca>
2019-02-26 09:01:27 -0500

    可以移动的Debugger Panel

--- www/wcy3/debugger/panel.js
+++ www/wcy3/debugger/panel.js
@@ -23,13 +23,43 @@ TQDebugger.Panel = (function () {
       '<button onclick="debugger_audioMethod(3);">声音3</button>' +
       '<div id="id-debug-info">debug info</div>';
 
+    TQ.State.audioMethod = parseInt(TQ.Utility.readLocalStorage('audioMethod', 4));
     window.debugger_audioMethod = function(method) {
       TQ.State.audioMethod = method;
+      TQ.Utility.writeLocalStorage('audioMethod', TQ.State.audioMethod);
     };
 
     panel = TQ.DomUtility.createElement(document.body, 'div', 'id-debug-panel', 'debug-panel-layer');
     panel.innerHTML = htmlStr;
     TQ.DomUtility.showElement(panel);
+    panel.addEventListener("touchstart", onTouchStart, false);
+    panel.addEventListener("touchmove", onTouchMove, false);
+    var startX, startY,
+      startTop, startLeft;
+    startTop = TQ.Utility.readLocalStorage('startTop', 0);
+    startLeft = TQ.Utility.readLocalStorage('startLeft', 0);
+    panel.style.left = startLeft + 'px';
+    panel.style.top = startTop + 'px';
+
+    function onTouchStart(evt) {
+      var touch = evt.touches[0];
+      if (touch) {
+        startX = touch.clientX;
+        startY = touch.clientY;
+        startTop = TQ.Utility.getCssSize(panel.style.top);
+        startLeft = TQ.Utility.getCssSize(panel.style.left);
+      }
+    }
+
+    function onTouchMove(evt) {
+      var touch = evt.touches[0];
+      if (touch) {
+        deltaX = touch.clientX - startX;
+        deltaY = touch.clientY - startY;
+        panel.style.left = startLeft + deltaX + 'px';
+        panel.style.top = startTop + deltaY + 'px';
+      }
+    }
   }
 
   function open() {
@@ -47,4 +77,6 @@ TQDebugger.Panel = (function () {
 
     eleLog.innerHTML += '<p>' + msg + '</p>';
   }
+
+
 }());
