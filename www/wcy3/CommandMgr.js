d429b6de21f088e8f71cadbcda3dcf8c773fc5f4
andrew.zhi <andrew.zhi@mobilelive.ca>
2019-06-26 09:00:09 -0400

    解决bug： 删除的时候，出现exception: 2 CompositeCommand的mouseup 避免注册多个listener

--- www/wcy3/CommandMgr.js
+++ www/wcy3/CommandMgr.js
@@ -93,7 +93,7 @@ window.TQ = window.TQ || {};
     CompositeCommand.open = function() {
         if (!__openedComposite) {
             __openedComposite = new CompositeCommand();
-            $(document).mouseup(CompositeCommand.close);
+            document.addEventListener('mouseup', CompositeCommand.close);
         }
     };
 
@@ -101,6 +101,7 @@ window.TQ = window.TQ || {};
         if (!__openedComposite) {
             return;
         }
+        document.removeEventListener('mouseup', CompositeCommand.close);
         TQ.CommandMgr.addToUndoStack(__openedComposite);
         __openedComposite = null;
     };
