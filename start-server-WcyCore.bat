9ec47276fd77608678032fb8943a6cd4aadff1f7
andrew <andrewzhi168@hotmail.com>
2022-01-21 21:01:07 -0500

    启动方法， 更新为：start-server-qianren

--- start-server-WcyCore.bat
+++
@@ -1,6 +0,0 @@
-start db-start.bat
-rem "启动了共用的数据库“
-rem ”开始启动服务器（包括web，api和后台）。。。。"
-e:
-cd E:\projects\WcyCore
-node backOffice\bin\www
--- start-server-eCard2.bat
+++
@@ -1,9 +0,0 @@
-start db-start.bat
-cd E:\projects\cardforvote
-e:
-node backOffice\bin\www
-
-echo on
-rem "启动共用的数据库“
-rem ”启动服务器（包括web，api和后台）"
-pause
