0bfada491047fe2526ff86756d2793d2e0c408dc
andrew <andrewzhi168@hotmail.com>
2023-02-12 06:25:04 -0500

    升级到CreateJS V1.0.0，其中，_hasMouseHandler和_matrix可能是本人以前扩充的，但是未抽取出来。所以，再次补充

--- www/wcy3/graphicsAdapters/easeljs/DisplayObjectExt.js
+++ www/wcy3/graphicsAdapters/easeljs/DisplayObjectExt.js
@@ -5,6 +5,7 @@
 (function () {
   var p = createjs.DisplayObject.prototype;
   // extended method
+  p._matrix = new createjs.Matrix2D;
   p.getWidth = function () {
     return 10;
   };
@@ -27,6 +28,23 @@
 
   p.naturalWidth = function () {
     return 0;
+  };
+
+  /**
+ * 是否拥有mouse Handler
+ * @param {*} mouseEvents 
+ * @returns 
+ */
+  p._hasMouseHandler = function (mouseEvents) {
+    var listeners = this._listeners;
+    return !!(mouseEvents & 1 &&
+
+      (this.onPress || this.onClick || this.onDoubleClick || listeners
+        && (this.hasEventListener("mousedown") || this.hasEventListener("click")
+          || this.hasEventListener("dblclick"))) ||
+      mouseEvents & 2 &&
+      (this.onMouseOver || this.onMouseOut || this.cursor || listeners
+        && (this.hasEventListener("mouseover") || this.hasEventListener("mouseout"))))
   };
 
 }());
