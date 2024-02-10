111ec3ede1d82d1115da8ddf2459f4ba88459e52
andrew <andrewzhi168@hotmail.com>
2024-02-10 14:07:23 -0500

    工具改进： 可以安装到任意磁盘，不限制安装盘是E。

--- backOffice/releaseOne.bat
+++ backOffice/releaseOne.bat
@@ -1,5 +1,5 @@
-set src=E:\proj\WcyCore2
-set dst=E:\proj\udoido3\%1
+set src=\proj\WcyCore2
+set dst=\proj\udoido3\%1
 
 md dst
 xcopy %src%\backOffice\*.*   %dst% /EXCLUDE:%src%\excludefiles.txt /s/e/v/y
