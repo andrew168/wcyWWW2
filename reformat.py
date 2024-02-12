81197b1d6fcbea05c78920d396474bb2004e869b
andrew <andrewzhi168@hotmail.com>
2024-02-12 10:10:39 -0500

    精简 eslint的输出 ignore # wx的library

--- reformat.py
+++ reformat.py
@@ -1,23 +1,27 @@
 import os;
 
-projectFolders=["."]
+#projectFolders=["."]
+# projectFolders=["backOffice"]
+projectFolders=["www"]
 
 def onedir(dirname): 
     blacklist = ['.vs', '.vscode', '.idea', '.git', 'dist', 'plugins', 'installationGuide',
                  'wwwKs',
-                 'node_modules', 'lib', 'lib-debug-duplicated', 'lib-debug', 'libDefer'];
+                 'node_modules', 'lib', 'lib-debug-duplicated', 'lib-debug', 'libDefer',
+                 'jweixin-1.0.0.js'];
     for fname in os.listdir(dirname):
         fullname = dirname + "\\" + fname;
-        print(fullname)
+        #print(fullname)
         if os.path.isdir(fullname) :
-           if (fname in blacklist): 
-              print("skip folder: " + fname);
-           else:
+           if (fname not in blacklist): 
               onedir(fullname);
+            #else :
+              #print("skip folder: " + fname);
+
         else:
-          if fname.endswith(".js") or fname.endswith(".html")  or fname.endswith(".json") and not fname.endswith(".min.js"):
+          if fname.endswith(".js") or fname.endswith(".html") and not fname.endswith(".min.js"):
              os.system("npx eslint --fix " + fullname);
-          else:
-             print("skip non-js file: " + fname) 
+          #else:
+             #print("skip non-js file: " + fname) 
 
 onedir(projectFolders[0]);    
